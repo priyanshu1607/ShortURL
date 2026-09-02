3e3f4cd6894b097e03e8bed5125b6a7b840f57b6
priyanshu1607 <priyanshumathur215@gmail.com>
2026-09-02 23:25:04 +0530

    removing USer

--- database/models.py
+++ database/models.py
@@ -1,21 +1,10 @@
-from pydantic import BaseModel, EmailStr, HttpUrl
+from pydantic import BaseModel, HttpUrl
 from typing import Optional
 from datetime import datetime
 
-
-class UserCreate(BaseModel):
-    name: str
-    email: EmailStr
-
-
-class UserResponse(BaseModel):
-    id: str
-    name: str
-    email: str
-    
 class URL(BaseModel):
     _id:int
-    shortURL: str
+    shortURL: str 
     Longurl: HttpUrl
     alies : Optional[str] = None
     click : int = 0
