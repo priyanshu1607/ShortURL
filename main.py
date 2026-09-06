ecbf8fe6c3a03a01a8026aa2c50f19392a110818
priyanshu1607 <priyanshumathur215@gmail.com>
2026-09-06 16:34:39 +0530

    updates

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["http://localhost:5173", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
